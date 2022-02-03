cc568eb1f42913fa3a366604a22235f363852509
Chris <26232872+crs-k@users.noreply.github.com>
2022-02-03 17:40:47 -0500

    Added custom error pages

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+
+const Custom404 = () => (
+  <Layout title="404">
+    <h1>404 - Page Not Found</h1>
+    <p>
+      <Link href="/">
+        <a>Go home</a>
+      </Link>
+    </p>
+  </Layout>
+);
+
+export default Custom404;
